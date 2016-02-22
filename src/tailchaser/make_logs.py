f7008140c160bf563c74143416fc354e1b658cee
thanos <thanosv@gmail.com>
2016-02-21 19:01:48 -0500

    flaked code

--- src/tailchaser/make_logs.py
+++ src/tailchaser/make_logs.py
@@ -1,48 +1,57 @@
-import logging.handlers
-import logging
-import sys
-import os
-import time
 import argparse
+import logging
+import logging.handlers
+import os
+
+import time
+
 
 FORMAT = '%(asctime)s %(levelname)s  %(module)s %(process)d %(thread)d %(message)s'
-LOGNUM= 1
-MAX_LOGS_PER_DIR = max(LOGNUM/10,1)
+LOGNUM = 1
+MAX_LOGS_PER_DIR = max(LOGNUM / 10, 1)
 MAX_LOG_SIZE = 2**20
 MSG_SIZE = 100
 MAX_LOG_ENTRIES = MAX_LOG_SIZE
 
+
 def get_args():
-    parser = argparse.ArgumentParser(description='tests the various hackathon services',
-            prog='loggen',
-            usage= '%(prog)s [options] destination_path'
-            )
-    parser.add_argument('path_head',    help='path to store log files')
-    parser.add_argument('--lognum', type=int,  default=LOGNUM, help='number of logfiles to generate: %s' % LOGNUM)
-    parser.add_argument('--message_size', type=int,  default=MSG_SIZE, help='message size: %s' % MSG_SIZE)
-    parser.add_argument('--max_logs_per_dir', type=int,  default=MAX_LOGS_PER_DIR, help='number of logfiles per directory: %s' % MAX_LOGS_PER_DIR)
-    parser.add_argument('--max_log_size', type=int,  default=MAX_LOG_SIZE, help='max log size before you rotate: %s' % MAX_LOG_SIZE)
-    parser.add_argument('--max_log_entries', type=int,  default=MAX_LOG_ENTRIES, help='max log entries (0= unlimited) : %s' % MAX_LOG_ENTRIES)
-    parser.add_argument('--format',   default=FORMAT, help='flog foromat, default: %s' % FORMAT)
-    parser.add_argument('--gen',  action='store_true',  default=False, help='dry runs, default is: %s' % True )
-    parser.add_argument('--tail',  action='store_true',  default=False, help='dry runs, default is: %s' % False )
-    parser.add_argument('--dryrun',  action='store_true',  default=False, help='dry runs, default is: %s' % False )
-    parser.add_argument('--delete',  action='store_true',  default=False, help='deletes all test logs and their directories, default is: %s' % False )
+    parser = argparse.ArgumentParser(
+        description='tests the various hackathon services',
+        prog='loggen',
+        usage='%(prog)s [options] destination_path'
+    )
+    parser.add_argument('path_head', help='path to store log files')
+    parser.add_argument('--lognum', type=int, default=LOGNUM, help='number of logfiles to generate: %s' % LOGNUM)
+    parser.add_argument('--message_size', type=int, default=MSG_SIZE, help='message size: %s' % MSG_SIZE)
+    parser.add_argument('--max_logs_per_dir', type=int, default=MAX_LOGS_PER_DIR,
+                        help='number of logfiles per directory: %s' % MAX_LOGS_PER_DIR)
+    parser.add_argument('--max_log_size', type=int, default=MAX_LOG_SIZE,
+                        help='max log size before you rotate: %s' % MAX_LOG_SIZE)
+    parser.add_argument('--max_log_entries', type=int, default=MAX_LOG_ENTRIES,
+                        help='max log entries (0= unlimited) : %s' % MAX_LOG_ENTRIES)
+    parser.add_argument('--format', default=FORMAT, help='flog foromat, default: %s' % FORMAT)
+    parser.add_argument('--gen', action='store_true', default=False,
+                        help='dry runs, default is: %s' % True)
+    parser.add_argument('--tail', action='store_true', default=False,
+                        help='dry runs, default is: %s' % False)
+    parser.add_argument('--dryrun', action='store_true', default=False,
+                        help='dry runs, default is: %s' % False)
+    parser.add_argument('--delete', action='store_true', default=False,
+                        help='deletes all test logs and their directories, default is: %s' % False)
     return parser.parse_args()
 
 
-
-
-if __name__ =='__main__':
-    import sys, random
+if __name__ == '__main__':
+    import random
     args = get_args()
     abs_path = os.path.abspath(args.path_head)
-    if not os.path.exists(abs_path): os.makedirs(abs_path)
+    if not os.path.exists(abs_path):
+        os.makedirs(abs_path)
     logger = logging.getLogger()
     log_file = os.path.join(abs_path, "test.log")
-    handler = logging.handlers.RotatingFileHandler(log_file,  maxBytes= 400*1024, backupCount=20)
+    handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=400 * 1024, backupCount=20)
     handler.setFormatter(logging.Formatter(args.format))
     logger.addHandler(handler)
     for i in xrange(10**6):
-        logger.error("%d - %s", i+1, "X"* 128)
+        logger.error("%d - %s", i + 1, "X" * 128)
         time.sleep(random.uniform(.1, 1))
--- src/tailchaser/producers.py
+++ src/tailchaser/producers.py
@@ -4,24 +4,23 @@
 
 """
 
+import cPickle as pickle
+import glob
+import hashlib
+import os
+import re
+import sys
+import time
+from collections import namedtuple
+
 
 class Producer(object):
     """Base producer class.
 
     """
 
+SIG_SZ = 256
 
-import cPickle as pickle
-import os
-import sys
-from collections import namedtuple
-import unicodedata
-import re
-import time
-import glob
-import hashlib
-
-SIG_SZ=256
 
 def slugify(value):
     """
@@ -39,34 +38,38 @@ class Tailer(Producer):
         self.args.source_pattern = source_pattern
         self.args.verbose = verbose
         self.checkpoint_filename = self.make_checkpoint_filename(self.args.source_pattern)
-        
-            #print open(file_to_check, 'r').read(250)
 
     def handoff(self, file_tailed, checkpoint, record):
         if self.args.verbose:
                 path = os.path.join('testoutput', os.path.basename(file_tailed))
-                open(path,'a').write(record)
+                open(path, 'a').write(record)
                 print file_tailed, checkpoint, record
         return record
 
     def make_checkpoint_filename(self, source_pattern):
-        return os.path.join(os.path.basename(slugify(source_pattern+'.checkpoint'))) 
+        return os.path.join(os.path.basename(slugify(source_pattern + '.checkpoint')))
 
     def should_tail(self, file_to_check, checkpoint):
-        if self.args.verbose: print 'should_tail', file_to_check, checkpoint
+        if self.args.verbose:
+            print 'should_tail', file_to_check, checkpoint
         stat = os.stat(file_to_check)
-        if self.args.verbose: print stat
+        if self.args.verbose:
+            print stat
         sig = self.make_sig(file_to_check, stat)
         if not checkpoint:
-            if self.args.verbose: print 'No Checkpoit',
-            return file_to_check,  (sig,  stat.st_ctime, 0)
-        if self.args.verbose: print 'should_tail', file_to_check, checkpoint, sig == checkpoint[0]
+            if self.args.verbose:
+                print 'No Checkpoit',
+            return file_to_check, (sig, stat.st_ctime, 0)
+        if self.args.verbose:
+            print 'should_tail', file_to_check, checkpoint, sig == checkpoint[0]
         if sig == checkpoint[0] and checkpoint[2] < stat.st_size:
-            retval =   file_to_check, checkpoint
-            if self.args.verbose: print 'SIG the same', retval
+            retval = file_to_check, checkpoint
+            if self.args.verbose:
+                print 'SIG the same', retval
             return retval
         if stat.st_mtime > checkpoint[1]:
-            if self.args.verbose: print " Younger", file_to_check, (sig,  stat.st_mtime,  0)
+            if self.args.verbose:
+                print " Younger", file_to_check, (sig, stat.st_mtime, 0)
             return file_to_check, (sig, stat.st_mtime, 0)
         print 'skipping', file_to_check, (sig, stat.st_ctime, 0)
 
@@ -75,29 +78,35 @@ class Tailer(Producer):
 
     def load_checkpoint(self, checkpoint_filename):
         try:
-            sig, mtime,    offset =  pickle.load(open(checkpoint_filename))
-            if self.args.verbose: print 'loading', checkpoint_filename, (sig, mtime,    offset)
-            return  sig, mtime,    offset
-        except (IOError,EOFError, ValueError):
-            return '',0,0
+            sig, mtime, offset = pickle.load(open(checkpoint_filename))
+            if self.args.verbose:
+                print 'loading', checkpoint_filename, (sig, mtime, offset)
+            return sig, mtime, offset
+        except (IOError, EOFError, ValueError):
+            return '', 0, 0
 
     def save_checkpoint(self, checkpoint_filename, checkpoint):
-        if self.args.verbose:print 'dumping', checkpoint_filename, checkpoint
+        if self.args.verbose:
+            print 'dumping', checkpoint_filename, checkpoint
         return pickle.dump(checkpoint, open(checkpoint_filename, 'wb'))
 
     def tail(self):
         checkpoint = self.load_checkpoint(self.checkpoint_filename)
         while True:
             try:
-                print  glob.glob(self.args.source_pattern)
-                to_tail = filter(None, sorted((self.should_tail(file_to_check, checkpoint) for file_to_check in glob.glob(self.args.source_pattern)), key=lambda x: x[1][1] if x else x))
-                if self.args.verbose: print 'checkpoint', checkpoint
-                if self.args.verbose: print 'to_tail', to_tail
-                if self.args.verbose: time.sleep(5)
+                print glob.glob(self.args.source_pattern)
+                to_tail = filter(None, sorted((self.should_tail(file_to_check, checkpoint)
+                                 for file_to_check in glob.glob(self.args.source_pattern)), key=lambda x: x[1][1] if x else x))
+                if self.args.verbose:
+                    print 'checkpoint', checkpoint
+                if self.args.verbose:
+                    print 'to_tail', to_tail
+                if self.args.verbose:
+                    time.sleep(5)
                 file_to_tail, checkpoint = to_tail[0]
                 to_tail = to_tail[1:]
-                
-                #raw_input()
+
+                #  raw_input()
                 for offset, record in self.process(file_to_tail, checkpoint):
                     self.handoff(file_to_tail, checkpoint, record)
                     checkpoint = checkpoint[0], checkpoint[1], offset
@@ -111,13 +120,14 @@ class Tailer(Producer):
                 self.save_checkpoint(self.checkpoint_filename, checkpoint)
             time.sleep(5)
 
-    def process(self, filename, (sig, st_mtime,  offset)):
+    def process(self, filename, (sig, st_mtime, offset)):
         with open(filename, 'rb') as file_to_tail:
-            if self.args.verbose: print 'offset', offset
-            
+            if self.args.verbose:
+                print 'offset', offset
             file_to_tail.seek(offset, 0)
-            if self.args.verbose: print file_to_tail.tell()
-            #raw_input()
+            if self.args.verbose:
+                print file_to_tail.tell()
+            # raw_input()
             while True:
                 record = self.read_record(file_to_tail)
                 if not record:
@@ -129,6 +139,6 @@ class Tailer(Producer):
         return file_to_tail.readline()
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     print sys.argv[1]
     Tailer(sys.argv[1], verbose=True).tail()
