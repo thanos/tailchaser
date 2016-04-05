91e6e345a422c31d2d528b1b8cd8f8e9f6278e2f
Thanos Vassilakis <thanos@syntazo.com>
2016-04-04 22:56:14 -0400

    Bump version: 0.1.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='tailchaser',
-    version='0.1.1',
+    version='0.1.0',
     license='BSD',
     description='the ultimate tailer',
     long_description='%s\n%s' % (
