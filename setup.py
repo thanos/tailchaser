ee0ae5f9242347d6cb92d8b4ece27a42077695a7
thanos vassilakis <thanos@syntazo.com>
2016-05-01 14:42:26 -0400

    updated regex

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         'tail', 'watch', 'logs',
     ],
     install_requires=[
-        'regex==2016.4.8',
+        'regex==2016.04.25',
         'six==1.10.0'
     ],
     extras_require={
