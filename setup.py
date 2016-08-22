e829b0fb97b4d9b072ee2c5a8e6120a19d53e56a
thanos vassilakis <thanosv@gmail.com>
2016-08-22 13:35:29 -0400

    Bump version: 0.2.7 → 0.2.8

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='tailchaser',
-    version='0.2.7',
+    version='0.2.8',
     license='BSD',
     description='the ultimate tailer',
     long_description='%s\n%s' % (
